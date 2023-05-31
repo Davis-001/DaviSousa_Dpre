ee5a7931933391e8e6c2ae5d72438cc25944ff84
Davis-001 <davi.s@to.estudante.senai.br>
2023-05-31 17:50:17 -0300

    Modificações na navegação

---
+++ app/controllers/positions_controller.rb
@@ -0,0 +1,19 @@
+class PositionsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def creat
+  end
+
+  def update
+  end
+end
